01fddd98788957a26ad793970697e79eacc86743
Marcin Kowalczyk <marcinck80@gmail.com>
2023-12-09 23:38:26 +0100

    deleted all files

--- src/components/Title/Title.jsx
+++
@@ -1,5 +0,0 @@
-import css from '../ContactForm/ContactForm.module.css';
-
-export const Title = ({ title }) => {
-  return <h2 className={css.title}>{title}</h2>;
-};
--- src/redux/modalContactDetail/modalContactDetail.selectors.js
+++
@@ -1,2 +0,0 @@
-export const selectIsOpenModal = state => state.modalStore.isOpenModal;
-export const selectModalData = state => state.modalStore.modalData;
